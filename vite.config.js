9089f7bb6b8616b4b659ebeb2decde9f6991174a
xmcp <xmcp@pku.edu.cn>
2024-08-03 01:04:45 +0800

    precompress built code

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import {defineConfig} from 'vite';
+import {compression} from 'vite-plugin-compression2';
+import zlib from 'zlib';
 
 export default defineConfig(() => {
     return {
         build: {
+            target: ['es2020', 'firefox78', 'chrome79', 'safari13'],
             outDir: 'build',
             sourcemap: false,
+            reportCompressedSize: false,
             rollupOptions: {
                 output: {
                     compact: true,
@@ -15,5 +19,17 @@ export default defineConfig(() => {
         esbuild: {
             legalComments: 'none',
         },
+        plugins: [
+            compression({
+                include: /\.*$/,
+                algorithm: 'brotliCompress',
+                compressionOptions: {
+                    params: {
+                        [zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
+                        [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
+                    },
+                },
+            }),
+        ],
     };
 });
